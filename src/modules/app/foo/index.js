85541b46f4b9f06a6c5eb21c93bd0f8171040524
Nickolay Lantinov <nickolay@primary.technology>
2015-09-07 18:51:23 +0300

    Test

--- src/modules/app/foo/index.js
+++ src/modules/app/foo/index.js
@@ -4,4 +4,11 @@ module.exports =
     angular.module('expressly.foo', [])
         .config(require('./routes.js'))
         .config(require('./i18n/en.js'))
-        .controller('barController', require('./controllers/bar.controller'));
+        .service('photoService', ['$http', '$q', require('./services/photo.service')])
+        .factory('Data', require('./factories/data.factory'))
+        .factory('Resource', require('./factories/resource.factory'))
+        .directive('ngInfiniteScroll', ['$timeout', 'Data', 'Resource', require('./directives/ng-infinite-scroll.directive')])
+        .controller('barController', require('./controllers/bar.controller'))
+        .controller('imageController', require('./controllers/image.controller'))
+        .controller('albumController', require('./controllers/album.controller'))
+        .controller('indexController', require('./controllers/index.controller'));
--- src/modules/app/foo/routes.js
+++ src/modules/app/foo/routes.js
@@ -3,9 +3,17 @@
 module.exports = /*ngInject*/
     function ($routeProvider) {
         $routeProvider
-            .when('/bar', {
-                templateUrl: 'app/foo/templates/bar.html',
-                controller: 'barController'
+            .when('/', {
+                templateUrl: 'app/foo/templates/index.html',
+                controller: 'indexController'
+            })
+            .when('/album/:id', {
+                templateUrl: 'app/foo/templates/album.html',
+                controller: 'albumController'
+            })
+            .when('/image/:id', {
+                templateUrl: 'app/foo/templates/image.html',
+                controller: 'imageController'
             })
             .otherwise({
                 redirectTo: '/bar'
